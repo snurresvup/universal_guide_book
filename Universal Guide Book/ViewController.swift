6fe9060442eafed051a44bf5d1dfa5e3176dea40
Benjamin Blankholm <benjamin.blankholm@gmail.com>
2017-07-13 09:17:03 +0200

    Initial Commit

---
+++ Universal Guide Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Universal Guide Book
+//
+//  Created by Benajmin Blankholm on 13/07/2017.
+//  Copyright © 2017 Benajmin Blankholm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
